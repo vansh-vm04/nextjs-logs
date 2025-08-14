a482551f2d74600392061fbc150d6a0cca8586b3
Vansh <vanshmorwani1@gmail.com>
2025-08-14 23:07:39 +0530

    prisma connection fixed

--- next-2/app/api/v1/signup/route.ts
+++ next-2/app/api/v1/signup/route.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
 
-  await prismaClient.user.create({
+  await prisma.user.create({
     data: { username: data.username, password: data.password },
   });
 
